5c467ef241d6afab1912e5c11130e172515f3344
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-11 12:49:41 +0100

    Info icon not showing up

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -6,12 +6,15 @@
       <template v-slot:prepend>
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
       </template>
-    
       <v-app-bar-title>Application Bar</v-app-bar-title>
-        <ul>
-          <li><Router-link to="/">Home</Router-link></li>
-          <li><Router-link to="/about">About</Router-link></li>
-          <li><Router-link to="/contact">Contact</Router-link></li>
-        </ul>
+      <v-button icon>
+        <Router-link to="/"><v-icon>mdi-home</v-icon>Home</Router-link>
+      </v-button>
+      <v-button icon>
+        <Router-link to="/about"><v-icon>mdi-info</v-icon>About</Router-link>
+      </v-button>
+      <v-button icon>
+        <Router-link to="/contact"><v-icon>mdi-email</v-icon>Contact</Router-link>
+      </v-button>
     </v-app-bar>
 </template>
