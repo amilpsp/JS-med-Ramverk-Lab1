b2122ba6030b490515ee1ac775b91893a3f01762
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-11 22:54:49 +0100

    removed about view

--- src/router/router.js
+++ src/router/router.js
@@ -1,16 +1,11 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 
-import AboutViewVue from '@/views/AboutView.vue'
 import ContactViewVue from '@/views/ContactView.vue'
 import HomeViewVue from '@/views/HomeView.vue'
 
 export default createRouter({
     history: createWebHashHistory(),
     routes:[
-        {
-            component: AboutViewVue,
-            path: '/about'
-        },
         {
             component: ContactViewVue,
             path: '/contact'
