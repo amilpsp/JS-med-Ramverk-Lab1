639666202bfc276e762896508b7e0b2b07ab73c1
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-13 19:24:01 +0100

    linter

--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,18 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import { createRouter, createWebHashHistory } from "vue-router";
 
-import ContactViewVue from '@/views/ContactView.vue'
-import HomeViewVue from '@/views/HomeView.vue'
+import ContactViewVue from "@/views/ContactView.vue";
+import HomeViewVue from "@/views/HomeView.vue";
 
 export default createRouter({
-    history: createWebHashHistory(),
-    routes:[
-        {
-            component: ContactViewVue,
-            path: '/contact'
-        },
-        {
-            component: HomeViewVue,
-            path:'/'
-        }
-    ]
-})
+  history: createWebHashHistory(),
+  routes: [
+    {
+      component: ContactViewVue,
+      path: "/contact",
+    },
+    {
+      component: HomeViewVue,
+      path: "/",
+    },
+  ],
+});
