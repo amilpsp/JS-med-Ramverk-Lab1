016e105f8637b1618a8f9fe03500debb51e1035f
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-13 18:40:20 +0100

    product cards

---
+++ src/components/Store.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-col cols="auto" class="align-items-center">
+    <v-card min-width="300" max-width="344">
+      <v-img src="../assets/img/combat-map.jpeg" height="200px" cover></v-img>
+
+      <v-card-title> {{ product.title }} </v-card-title>
+
+      <v-card-subtitle> {{ product.description }} </v-card-subtitle>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
+<!-- /* {
+      "id": "",
+      "title": "",
+      "brand": "",
+      "author": "",
+      "category": "",
+      "price": "",
+      "sizes": ["xs", "s", "m", "l", "xl"],
+      "img-src": "",
+      "alt": "",
+      "best-seller": "false",
+      "available": "true"
+    } */ -->
