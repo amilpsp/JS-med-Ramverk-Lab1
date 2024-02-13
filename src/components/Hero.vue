3f44f84a16e5ee5fb9514fed24608d19a98cf56f
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-13 13:01:23 +0100

    renamed HelloWorld to Hero to reuse

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,10 +1,9 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-      
       <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+      <h1 class="text-h2 font-weight-bold">NURDS</h1>
 
       <div class="py-14" />
 
@@ -17,33 +16,9 @@
             target="_blank"
             variant="text"
           >
-            <v-icon
-              icon="mdi-view-dashboard"
-              size="large"
-              start
-            />
+            <v-icon icon="mdi-view-dashboard" size="large" start />
 
             Components
-          </v-btn>
-        </v-col>
-
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            target="_blank"
-            variant="flat"
-          >
-            <v-icon
-              icon="mdi-speedometer"
-              size="large"
-              start
-            />
-
-            Get Started
           </v-btn>
         </v-col>
 
@@ -55,11 +30,7 @@
             target="_blank"
             variant="text"
           >
-            <v-icon
-              icon="mdi-account-group"
-              size="large"
-              start
-            />
+            <v-icon icon="mdi-account-group" size="large" start />
 
             Community
           </v-btn>
@@ -70,5 +41,5 @@
 </template>
 
 <script setup>
-  //
+//
 </script>
