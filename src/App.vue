a63db107afa488aa32b602e58320f202dd12fc15
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-13 18:43:18 +0100

    Nav and Footer placement corrected
    Were put into <v-main> by mistake

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <v-app>
     <v-main>
-      <Nav/>
-      <RouterView/>
-      <Footer/>
+      <Nav />
+      <RouterView />
+      <Footer />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import Footer from './components/Footer.vue';
+import Footer from "./components/Footer.vue";
+import Nav from "./components/Nav.vue";
 </script>
