e4f5ee268a0b8f0ef5182629f65c34467c192be9
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-13 18:41:01 +0100

    BUG: fill-height
    Adding the store component to the view broke it

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,35 @@
 <script setup>
 import Hero from "@/components/Hero.vue";
+import Store from "@/components/Store.vue";
 </script>
 <template>
   <Hero />
+  <v-row>
+    <Store v-for="product in products" :key="product.id" :product="product" />
+  </v-row>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  name: "HomeView",
+  components: { store: Store },
+  created() {
+    this.fetchProducts();
+  },
+  methods: {
+    fetchProducts() {
+      fetch("../src/products.json")
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+          this.products = result;
+        });
+    },
+  },
+};
+</script>
+<style></style>
