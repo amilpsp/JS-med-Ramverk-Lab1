29b3cb48597dd9a0c39885aa7c464b4e2cf18d6e
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-15 00:46:01 +0100

    added card action + related script
    To do: find out how to add card-tittle text-wrap while keeping uniform card size

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,17 +1,44 @@
 <template>
   <v-col cols="3">
     <v-card>
-      <v-img :src="cardImgSrc" height="200px" cover />
+      <v-img :src="cardImgSrc" cover />
 
-      <v-card-title> {{ product.title }} </v-card-title>
+      <v-card-title>
+        <!-- <v-card-title class="text-wrap"> -->
+        {{ product.title }}
+      </v-card-title>
 
-      <v-card-subtitle> {{ product.description }} </v-card-subtitle>
+      <v-card-subtitle>SEK {{ product.price }}</v-card-subtitle>
+
+      <v-card-actions>
+        <v-btn color="orange-lighten-2" variant="text"> Read more </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+          @click="show = !show"
+        ></v-btn>
+      </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+
+          <v-card-text>
+            {{ product.description }}
+          </v-card-text>
+        </div>
+      </v-expand-transition>
     </v-card>
   </v-col>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    show: false,
+  }),
   props: {
     product: {
       required: true,
