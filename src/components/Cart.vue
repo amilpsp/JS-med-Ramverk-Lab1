c5d6079dbcd43278b3cc128f5a3510de9415e692
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-19 21:20:11 +0100

    Checkout page+ 2 components

---
+++ src/components/Cart.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-table fixed-header fixed-footer theme="dark">
+    <thead>
+      <tr>
+        <th class="text-left">Product Name</th>
+        <th class="text-left">Price</th>
+        <th class="text-left">Amount</th>
+        <th class="text-left">Sum</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in desserts" :key="item.name">
+        <td>{{ item.name }}</td>
+        <td>{{ item.calories }}</td>
+        <td>{{ item.amount }}</td>
+        <td>{{ item.sum }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          amount: 159,
+          sum: 159,
+        },
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          amount: 159,
+          sum: 159,
+        },
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          amount: 159,
+          sum: 159,
+        },
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          amount: 159,
+          sum: 159,
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,1 +1,16 @@
-<template></template>
+<script setup>
+import CheckoutForm from "@/components/CheckoutForm.vue";
+import Cart from "@/components/Cart.vue";
+</script>
+<template>
+  <v-row class="my-4">
+    <v-col class="ml-3">
+      <h1 class="text-center">Your Cart</h1>
+      <Cart></Cart>
+    </v-col>
+    <v-col class="mr-3" cols="4">
+      <h1 class="text-center">Checkout</h1>
+      <CheckoutForm></CheckoutForm>
+    </v-col>
+  </v-row>
+</template>
