ad099f7f0bafc7727aed8907eacfccd8af7b7869
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-19 21:52:36 +0100

    text alignments for the table

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -3,17 +3,17 @@
     <thead>
       <tr>
         <th class="text-left">Product Name</th>
-        <th class="text-left">Price</th>
-        <th class="text-left">Amount</th>
-        <th class="text-left">Sum</th>
+        <th class="text-center">Price</th>
+        <th class="text-center">Amount</th>
+        <th class="text-right">Sum</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="item in desserts" :key="item.name">
-        <td>{{ item.name }}</td>
-        <td>{{ item.calories }}</td>
-        <td>{{ item.amount }}</td>
-        <td>{{ item.sum }}</td>
+        <td class="text-left">{{ item.name }}</td>
+        <td class="text-center">{{ item.calories }}</td>
+        <td class="text-center">{{ item.amount }}</td>
+        <td class="text-right">{{ item.sum }}</td>
       </tr>
     </tbody>
   </v-table>
